b271c1a03288f2f8e87d3f6c792665b07cf38bc0
danielreeder <101375530+danielreeder@users.noreply.github.com>
2024-07-19 22:50:50 -0700

    data

--- python/team.py
+++ python/team.py
@@ -2,7 +2,6 @@ import pandas as pd
 import requests as requests
 from bs4 import BeautifulSoup as bs
 import os
-import time
 
 class Team:
     def __init__(self, name, path=''):
@@ -21,9 +20,8 @@ class Team:
         print("Data grabbed")
 
         soup = bs(r.content, 'html.parser')
-
+        
         tables = soup.find_all('div', class_='table_wrapper')
-        # tables = soup.find_all('div')
 
         batting_header, batting_data = self.parse_data(tables[0])
         pitching_header, pitching_data = self.parse_data(tables[1])
@@ -106,6 +104,59 @@ teams = {
     "Washington Nationals": "WAS"
 }
 
-for team in teams:
-    Team(teams[team]).get_data()
-    time.sleep(4)
+def calculate_probability(prob_of_reaching, prob_of_next):
+    return prob_of_reaching * prob_of_next
+
+def calculate_win_probability(inning, outs, home_score, away_score, bases):
+    return calculate_win_probability_wrapper(inning, outs, bases, home_score, away_score, 1, 1)
+
+""" BASES """
+""" 1 : 000 """
+""" 2 : 001 """
+""" 3 : 010 """
+""" 4 : 011 """
+""" 5 : 100 """
+""" 6 : 101 """
+""" 7 : 110 """
+""" 8 : 111 """
+def calculate_win_probability_wrapper(inning, outs, bases, home_score, away_score, current_probability, next_action_probability):
+    if current_probability < 1e-4:
+        return current_probability * int(home_score > away_score)   
+    cumulative_win_probability = 0.0
+    # SINGLE
+
+    # DOUBLE
+
+    # TRIPLE
+
+    # HOME RUN
+
+    # STRIKEOUT
+    strikeout_probability = ...
+    if outs == 2:
+        inning += 1
+        outs = 0
+        bases = [0, 0, 0]
+    else:
+        outs += 1
+    cumulative_win_probability += calculate_win_probability_wrapper(inning, outs, bases, home_score, away_score, current_probability, strikeout_probability)
+
+    # FLYOUT
+    if bases[0] == 1:
+        bases[0] = 0
+        if inning % 2 == 0:
+            home_score += 1
+        else: 
+            away_score += 1
+    
+    if bases[1] == 1:
+        bases[0] = 1
+    
+    if outs == 2:
+        inning += 1
+        outs = 0
+        bases = [0, 0, 0]
+    
+    cumulative_win_probability += calculate_win_probability_wrapper(inning, outs, bases, home_score, away_score, current_probability, strikeout_probability)
+
+    # GB OUT
